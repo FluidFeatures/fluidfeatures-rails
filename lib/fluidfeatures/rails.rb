061596e4dc342874d114ad2e4fe1a0d213f64c7e
Phil Whelan <phil123@gmail.com>
2012-10-30 17:50:41 -0700

    Move fluidfeatures generic code into fluidfeatures/client.rb

--- lib/fluidfeatures/rails.rb
+++ lib/fluidfeatures/rails.rb
@@ -3,11 +3,14 @@ if RUBY_VERSION < "1.9.2"
   require "pre_ruby192/uri"
 end
 
+require "fluidfeatures/client"
+
 module FluidFeatures
   module Rails
 
     class << self
       attr_accessor :enabled
+      attr_accessor :client
     end
     
     #
@@ -38,200 +41,26 @@ module FluidFeatures
 
       ::Rails::Application.initializer "fluidfeatures.initializer" do
         ActiveSupport.on_load(:action_controller) do
+          api_baseuri = ENV["FLUIDFEATURES_BASEURI"]
+          api_appid   = ENV["FLUIDFEATURES_APPID"]
+          api_secret  = ENV["FLUIDFEATURES_SECRET"]
 
-          @@baseuri = ENV["FLUIDFEATURES_BASEURI"]
-          @@secret = ENV["FLUIDFEATURES_SECRET"]
-          @@app_id = ENV["FLUIDFEATURES_APPID"]
-          @@http = PersistentHTTP.new(
-            :name         => 'fluidfeatures',
-            :logger       => ::Rails.logger,
-            :pool_size    => 10,
-            :warn_timeout => 0.25,
-            :force_retry  => true,
-            :url          => @@baseuri
+          ::FluidFeatures::Rails.client = ::FluidFeatures::Client.new(
+            api_baseuri,
+            api_appid,
+            api_secret,
+            # options
+            {
+              :logger => nil,#::Rails.logger
+            }
           )
-          @@unknown_features = {}
-          @@last_fetch_duration = nil
 
           ActionController::Base.append_before_filter :fluidfeatures_request_before
           ActionController::Base.append_after_filter  :fluidfeatures_request_after
-
         end
       end
 
       @enabled = true
-    end
-    
-    #
-    # This can be used to control how much of your user-base sees a
-    # particular feature. It may be easier to use the dashboard provided
-    # at https://www.fluidfeatures.com/dashboard to manage this, or to
-    # set timers to automate the gradual rollout of your new features.
-    #
-    def self.feature_set_enabled_percent(feature_name, enabled_percent)
-      begin
-        uri = URI(@@baseuri + "/app/" + @@app_id.to_s + "/features/" + feature_name.to_s)
-        request = Net::HTTP::Put.new uri.path
-        request["Content-Type"] = "application/json"
-        request["Accept"] = "application/json"
-        request['AUTHORIZATION'] = @@secret
-        payload = {
-          :enabled => {
-            :percent => enabled_percent
-          }
-        }
-        request.body = JSON.dump(payload)
-        response = @@http.request uri, request
-        if response.is_a?(Net::HTTPSuccess)
-          ::Rails.logger.error "[" + response.code.to_s + "] Failed to set feature enabled percent : " + uri.to_s + " : " + response.body.to_s
-        end
-      rescue
-        ::Rails.logger.error "Request to set feature enabled percent failed : " + uri.to_s
-        raise
-      end
-    end
-
-    #
-    # Returns all the features that FluidFeatures knows about for
-    # your application. The enabled percentage (how much of your user-base)
-    # sees each feature is also provided.
-    #
-    def self.get_feature_set
-      features = nil
-      begin
-        uri = URI(@@baseuri + "/app/" + @@app_id.to_s + "/features")
-        request = Net::HTTP::Get.new uri.path
-        request["Accept"] = "application/json"
-        request['AUTHORIZATION'] = @@secret
-        response = @@http.request request
-        if response.is_a?(Net::HTTPSuccess)
-          features = JSON.parse(response.body)
-        end
-      rescue
-        ::Rails.logger.error "Request failed when getting feature set from " + uri.to_s
-        raise
-      end
-      if not features
-        ::Rails.logger.error "Empty feature set returned from " + uri.to_s
-      end
-      features
-    end
-
-    #
-    # Returns all the features enabled for a specific user.
-    # This will depend on the user_id and how many users each
-    # feature is enabled for.
-    #
-    def self.get_user_features(user)
-
-      if not user
-        raise "user object should be a Hash"
-      end
-      if not user[:id]
-        raise "user does not contain :id field"
-      end
-
-      # extract just attribute ids into simple hash
-      attribute_ids = {
-        :anonymous => !!user[:anonymous]
-      }
-      [:unique, :cohorts].each do |attr_type|
-        if user.has_key? attr_type
-          user[attr_type].each do |attr_key, attr|
-            if attr.is_a? Hash
-              if attr.has_key? :id
-                attribute_ids[attr_key] = attr[:id]
-              end
-            else
-              attribute_ids[attr_key] = attr
-            end
-          end
-        end
-      end
-
-      # normalize attributes ids as strings
-      attribute_ids.each do |attr_key, attr_id|
-        if attr_id.is_a? FalseClass or attr_id.is_a? TrueClass
-          attribute_ids[attr_key] = attr_id.to_s.downcase
-        elsif not attr_id.is_a? String
-          attribute_ids[attr_key] = attr_id.to_s
-        end
-      end
-
-      features = {}
-      fetch_start_time = Time.now
-      begin
-        uri = URI("#{@@baseuri}/app/#{@@app_id}/user/#{user[:id]}/features")
-        uri.query = URI.encode_www_form( attribute_ids )
-        url_path = uri.path
-        if uri.query
-          url_path += "?" + uri.query
-        end
-        request = Net::HTTP::Get.new url_path
-        request["Accept"] = "application/json"
-        request['AUTHORIZATION'] = @@secret
-        $stderr.puts "GET #{uri}"
-        response = @@http.request request
-        if response.is_a?(Net::HTTPSuccess)
-          features = JSON.parse(response.body)
-        else
-          ::Rails.logger.error "[#{response.code}] Failed to get user features : #{uri} : #{response.body}"
-        end
-      rescue
-        ::Rails.logger.error "Request to get user features failed : #{uri}"
-        raise
-      end
-      @@last_fetch_duration = Time.now - fetch_start_time
-      features
-    end
-    
-    #
-    # This is called when we encounter a feature_name that
-    # FluidFeatures has no record of for your application.
-    # This will be reported back to the FluidFeatures service so
-    # that it can populate your dashboard with this feature.
-    # The parameter "default_enabled" is a boolean that says whether
-    # this feature should be enabled to all users or no users.
-    # Usually, this is "true" for existing features that you are
-    # planning to phase out and "false" for new feature that you
-    # intend to phase in.
-    #
-    def self.unknown_feature_hit(feature_name, version_name, defaults)
-      if not @@unknown_features[feature_name]
-        @@unknown_features[feature_name] = { :versions => {} }
-      end
-      @@unknown_features[feature_name][:versions][version_name] = defaults
-    end
-    
-    #
-    # This reports back to FluidFeatures which features we
-    # encountered during this request, the request duration,
-    # and statistics on time spent talking to the FluidFeatures
-    # service. Any new features encountered will also be reported
-    # back with the default_enabled status (see unknown_feature_hit)
-    # so that FluidFeatures can auto-populate the dashboard.
-    #
-    def self.log_request(user_id, payload)
-      begin
-        (payload[:stats] ||= {})[:ff_latency] = @@last_fetch_duration
-        if @@unknown_features.size
-          (payload[:features] ||= {})[:unknown] = @@unknown_features
-          @@unknown_features = {}
-        end
-        uri = URI(@@baseuri + "/app/#{@@app_id}/user/#{user_id}/features/hit")
-        request = Net::HTTP::Post.new uri.path
-        request["Content-Type"] = "application/json"
-        request["Accept"] = "application/json"
-        request['AUTHORIZATION'] = @@secret
-        request.body = JSON.dump(payload)
-        response = @@http.request request
-        unless response.is_a?(Net::HTTPSuccess)
-          ::Rails.logger.error "[" + response.code.to_s + "] Failed to log features hit : " + uri.to_s + " : " + response.body.to_s
-        end
-      rescue Exception => e
-        ::Rails.logger.error "Request to log user features hit failed : " + uri.to_s
-        raise
-      end
     end
     
   end
@@ -326,7 +155,7 @@ module ActionController
           options.remove(:version)
         end
         ::Rails.logger.debug "fluidfeature: seeing feature '#{feature_name.to_s}' (version '#{version_name.to_s}') for the first time."
-        FluidFeatures::Rails.unknown_feature_hit(feature_name, version_name, options)
+        ::FluidFeatures::Rails.client.unknown_feature_hit(feature_name, version_name, options)
       end
       if enabled
         @ff_features_hit[feature_name] ||= {}
@@ -357,7 +186,7 @@ module ActionController
     #
     def fluidfeatures_retrieve_user_features
       user = fluidfeatures_user
-      @ff_features = FluidFeatures::Rails.get_user_features(user)
+      @ff_features = ::FluidFeatures::Rails.client.get_user_features(user)
     end
      
     #
@@ -390,7 +219,7 @@ module ActionController
           (payload[:user] ||= {})[key] = fluidfeatures_user[key]
         end
       end
-      FluidFeatures::Rails.log_request(fluidfeatures_user[:id], payload)
+      ::FluidFeatures::Rails.client.log_request(fluidfeatures_user[:id], payload)
     end
     
     def fluidfeatures_defaults
