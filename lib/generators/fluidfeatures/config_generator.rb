96446d5d87bae9d556a5b04a34396f5053a232ae
Phil Whelan <phil123@gmail.com>
2013-01-12 10:14:45 -0800

    update yaml config generator to re-use previous yaml config

--- lib/generators/fluidfeatures/config_generator.rb
+++ lib/generators/fluidfeatures/config_generator.rb
@@ -5,47 +5,59 @@ module Fluidfeatures
 
     def create_initializer_file
 
+      if File.file? CONFIG_FILE
+        config = YAML.load_file(CONFIG_FILE)
+      else
+        config = {
+          "common" => {
+            "base_uri" => "https://www.fluidfeatures.com/service",
+            "cache" => {
+              "enable" => false,
+              "dir" => "tmp/fluidfeatures",
+              "limit" => "10MB"
+            },
+          },
+          "development" => {},
+          "production" => {},
+          "test" => {},
+        }
+      end
+
       puts "Enter FluidFeatures credentials [press enter to skip]"
-      print "development app_id: "
-      dev_app_id = gets.chomp
+      ["development", "production", "test"].each do |env|
+        config[env] ||= {}
+        ["app_id", "secret", "base_uri"].each do |key|
+          if config[env][key] and config[env][key].size > 0
+            default = config[env][key]
+          elsif config["common"][key] and config["common"][key].size
+            default = config["common"][key]
+          end
+          print "%s %s %s: " % [
+            env,
+            key,
+            default ? "[#{default}]" : ""
+          ]
+          value = gets.chomp.strip
+          if value and value.size > 0
+            config[env][key] = value
+          elsif default
+            config[env][key] = default
+          else
+            config[env][key] = nil
+          end
+        end
+        if config[env]["base_uri"] == config["common"]["base_uri"]
+          config[env].delete("base_uri")
+        end
+      end
 
-      print "development secret: "
-      dev_secret = gets.chomp
+      yaml = YAML.dump(config)
+      # remove "---" at top
+      yaml.sub!(/^---\r?\n/, "")
+      # add extra line between environments
+      yaml.gsub!(/(\r?\n)(\S)/) {|m| $1 + $1 + $2 }
 
-      print "production app_id: "
-      prod_app_id = gets.chomp
-
-      print "production secret: "
-      prod_secret = gets.chomp
-
-      print "test app_id: "
-      test_app_id = gets.chomp
-
-      print "test secret: "
-      test_secret = gets.chomp
-
-      puts "Warning: production app_id and secret have not been set in #{CONFIG_FILE}"
-
-      create_file CONFIG_FILE, """
-common:
-  base_uri: https://www.fluidfeatures.com/service
-  cache:
-    enable: false
-    dir: tmp/fluidfeatures
-    limit: 2mb
-
-development:
-  app_id: #{dev_app_id}
-  secret: #{dev_secret}
-
-test:
-  app_id: #{test_app_id}
-  secret: #{test_secret}
-
-production:
-  app_id: #{prod_app_id}
-  secret: #{prod_secret}
-"""
+      create_file CONFIG_FILE, yaml
     end
   end
 end
